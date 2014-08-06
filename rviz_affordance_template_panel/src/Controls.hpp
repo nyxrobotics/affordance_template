ef741ba104b57499769830b7ebdc706b86738699
Paul Dinh <paul.dinh@nasa.gov>
2014-08-06 17:15:39 -0500

    Cleaned up Controls class of TemplatePanel

--- rviz_affordance_template_panel/src/Controls.hpp
+++ rviz_affordance_template_panel/src/Controls.hpp
@@ -4,15 +4,12 @@
 #include <iostream>
 #include <boost/shared_ptr.hpp>
 
-/* qt */
 #include <QTableWidgetItem>
 
-/* Project Include */
 #include "RobotConfig.hpp"
 #include "util.hpp"
 #include "ui_rviz_affordance_template_panel.h"
 
-// zmq
 #include <zmq.hpp>
 #include "AffordanceTemplateServerCmd.pb.h"
 
@@ -26,62 +23,29 @@ namespace rviz_affordance_template_panel
     {
     public:
     	typedef boost::shared_ptr<RobotConfig> RobotConfigSharedPtr;
-        // Constructors
         Controls(Ui::RVizAffordanceTemplatePanel* ui);
-        ~Controls();
+        ~Controls() {};
 
-        /** \brief Go To Start Command.
-         */
         void go_to_start();
-
-        /** \brief Go To End Command.
-         */
         void go_to_end();
-
-        /** \brief Pause Command.
-         */
         void pause();
-
-        /** \brief Stop Command.
-         */
         void stop();
-
-        /** \brief Play Backward Command.
-         */
         void play_backward();
-
-        /** \brief Play Forward Command.
-         */
         void play_forward();
-
-        /** \brief Step Backward Command.
-         */
         void step_backward();
-
-        /** \brief Step Forward Command.
-         */
         void step_forward();
 
-        void setConnected(bool value);
-        void setRobotMap(std::map<std::string, RobotConfigSharedPtr> robotMap);
-        void setRobotName(std::string name);
-        void setSocket(zmq::socket_t* socket);
+        void setConnected(bool value) { connected = value; };
+        void setRobotMap(std::map<std::string, RobotConfigSharedPtr> map) { robotMap = map; };
+        void setRobotName(std::string name) { robot_name = name; };
+        void setSocket(zmq::socket_t* sock) { socket = sock; };
 
     private:
-        // Pointer to ui.
         Ui::RVizAffordanceTemplatePanel* _ui;
-
         void send_request(const Request& request, Response& response, long timeout=1000000);
-
-        // void send_request(const Request& request, Response& response, long timeout=1000000);
-        // map to track instantiated object templates
         std::map<std::string, RobotConfigSharedPtr> robotMap;
         std::string robot_name;
-
         std::vector<std::string> getSelectedEndEffectors();
-
-        // zmq
-        // zmq::context_t context;
         zmq::socket_t* socket;
         bool connected;
     };
